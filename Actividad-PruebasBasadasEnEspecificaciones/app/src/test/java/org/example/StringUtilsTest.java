03b3c4d7f3c45d7242980536a22657257041d032
GaboYR <gabriel.yarleque.r@uni.pe>
2024-06-04 18:08:02 -0500

    Test class up to date

--- Actividad-PruebasBasadasEnEspecificaciones/app/src/test/java/org/example/StringUtilsTest.java
+++ Actividad-PruebasBasadasEnEspecificaciones/app/src/test/java/org/example/StringUtilsTest.java
@@ -26,52 +26,64 @@ class StringUtilsTest {
     // Ejercicio 3
     // Paramatro str
     // str como null
+    @Test
     void nullString() {
         assertThat(StringUtils.substringsBetween(null, "a", "d")).isEqualTo(new String[] { });
     }
     // str como vacío
+    @Test
     void emptyString() {
         assertThat(StringUtils.substringsBetween("", "a", "d")).isEqualTo(new String[] { });
     }
     // str con longitud 1
+    @Test
     void oneCharString() {
         assertThat(StringUtils.substringsBetween("a", "a", "d")).isEqualTo(new String[] { });
     }
     // str con longitud > 1
+    @Test
     void noOpenTag() {
         assertThat(StringUtils.substringsBetween("abcd", "a", "d")).isEqualTo(new String[] {"bc"});
     }
     // Parametro open
     // open como null
+    @Test
     void nullOpenTag() {
         assertThat(StringUtils.substringsBetween("abcd", null, "d")).isEqualTo(new String[] { });
     }
     // open como vacío
+    @Test
     void emptyOpenTag() {
         assertThat(StringUtils.substringsBetween("abcd", "", "d")).isEqualTo(new String[] { });
     }
     // open con longitud 1
+    @Test
     void oneCharOpenTag() {
         assertThat(StringUtils.substringsBetween("abcd", "a", "d")).isEqualTo(new String[] { "bc" });
     }
     // open con longitud > 1
+    @Test
     void openTagLongerThan1Char() {
         assertThat(StringUtils.substringsBetween("aabcddaabfddaab", "aa", "dd")).isEqualTo(new String[] { "bc", "bf" });
     }
     // Parametro close
     // close como null
+    @Test
     void nullCloseTag() {
         assertThat(StringUtils.substringsBetween("abcd", "a", null)).isEqualTo(new String[] { });
     }
     // close como vacío
+    @Test
     void emptyCloseTag() {
         assertThat(StringUtils.substringsBetween("abcd", "a", "")).isEqualTo(new String[] { });
     }
     // close con longitud 1
+    @Test
     void oneCharCloseTag() {
         assertThat(StringUtils.substringsBetween("abcd", "a", "d")).isEqualTo(new String[] { "bc" });
     }
     // close con longitud > 1
+    @Test
     void closeTagLongerThan1Char() {
         assertThat(StringUtils.substringsBetween("aabcddaabfddaab", "aa", "dd")).isEqualTo(new String[] { "bc", "bf" });
     }
